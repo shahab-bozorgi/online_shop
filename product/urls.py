1886cd6e08ee192317b209c5f9702807c6c8ff41
shahubak <shahab.bozorgi007@gmail.com>
2024-04-28 17:28:46 +0330

    filter

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     path('<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
-    path('navbar', views.NavbarView.as_view(), name='navbar')
+    path('navbar', views.NavbarView.as_view(), name='navbar'),
+    path('all', views.ProductList.as_view(), name='products_list'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
+
+
 from django.shortcuts import render
-from django.views.generic import DetailView, TemplateView
-from product.models import Product, Category
+from django.views.generic import DetailView, TemplateView, ListView
+from product.models import Product, Category, Color
 
 
 class ProductDetailView(DetailView):
@@ -13,6 +15,38 @@ class NavbarView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(NavbarView, self).get_context_data()
         context['categories'] = Category.objects.all()
+        return context
+
+
+
+class ProductList(ListView):
+    template_name = 'product/product_list.html'
+    queryset = Product.objects.all()
+    # context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        request = self.request
+        colors = request.GET.getlist('color')
+        size = request.GET.getlist('size')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+
+        queryset = Product.objects.all()
+        if colors:
+            queryset = queryset.filter(color__title__in=colors).distinct()
+
+
+        if size:
+            queryset = queryset.filter(size__title__in=size).distinct()
+
+        if min_price and max_price:
+            queryset = queryset.filter(price__lte=max_price, price__gte=min_price).distinct()
+
+
+        # print(colors, size, min_price, max_price)
+        context = super(ProductList, self).get_context_data()
+        context['object_list'] = queryset
+        context['colors'] = Color.objects.all()
         return context
 
 
